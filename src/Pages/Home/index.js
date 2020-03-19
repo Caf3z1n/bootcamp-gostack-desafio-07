ef56d48af0251324efd36aca475ebc600cec3726
Pedro Henrique <pedrosg2003@gmail.com>
2020-03-19 16:06:16 -0300

    First commit

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-venetto-casual-caminhada-leve-confortavel-masculino/06/EPL-0186-006/EPL-0186-006_zoom2.jpg?ts=1564484531&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADCIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-venetto-casual-caminhada-leve-confortavel-masculino/06/EPL-0186-006/EPL-0186-006_zoom2.jpg?ts=1564484531&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADCIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-venetto-casual-caminhada-leve-confortavel-masculino/06/EPL-0186-006/EPL-0186-006_zoom2.jpg?ts=1564484531&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADCIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-venetto-casual-caminhada-leve-confortavel-masculino/06/EPL-0186-006/EPL-0186-006_zoom2.jpg?ts=1564484531&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADCIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-venetto-casual-caminhada-leve-confortavel-masculino/06/EPL-0186-006/EPL-0186-006_zoom2.jpg?ts=1564484531&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADCIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-venetto-casual-caminhada-leve-confortavel-masculino/06/EPL-0186-006/EPL-0186-006_zoom2.jpg?ts=1564484531&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADCIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
